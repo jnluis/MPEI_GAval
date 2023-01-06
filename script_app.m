781bb07d4bd5de2910ea53cf4406fd0ab16c408a
Diana Miranda <dianarrmiranda@gmail.com>
2023-01-06 12:39:52 +0000

    opção 3 feita

--- script_app.m
+++ script_app.m
@@ -24,7 +24,7 @@ while(menu ~= 5 && menu ~= 0)
             fprintf('\nUsers who rate the movie "%s":\n',dic2{filmID});
             userIDs = Set{filmID};
             for i=1:length(userIDs)
-                fprintf(" (ID: %3d) %s %s\n", userIDs(i), dic{userIDs(i),2}, dic{userIDs(i),3})
+                fprintf(" (ID: %d) %s %s\n", userIDs(i), dic{userIDs(i),2}, dic{userIDs(i),3})
             end
         case 2
             nHF = 500;
@@ -49,39 +49,61 @@ while(menu ~= 5 && menu ~= 0)
                 c = setdiff(Set{x,:}, Set{filmID,:})';
                 usersInfo = [usersInfo, c];
             end
-            % Remove any duplicate recommendations.
+
             usersInfo = unique(usersInfo);
-            % Print the titles of the recommended movies.
+
             fprintf('Users who rated one of the similar movies, but not the movie "%s":\n', dic2{filmID});
             for i = usersInfo
-                fprintf(" (ID: %3d) %s %s\n", i, dic{i,2}, dic{i,3})
+                fprintf(" (ID: %d) %s %s\n", i, dic{i,2}, dic{i,3})
             end
 
         case 3
             userIDs = Set{filmID};
             threshold=0.9; % limiar da decisão para a Dist. de Jaccard
-            SimilarUsersByInterest = SimilarInterests(MinHashInterests, userIDs, threshold);
+            nHF = 100;
+            conjunto = [];
+            count = 1;
+            for i= 1:length(userIDs)
+                n1 = userIDs(i);  
+                for n2=1:nUsers
+                    if ~ismember(n2, Set{filmID}) % se o utilizador não viu o filme atual é feito o cálculo da distância
+                        distanceInterests = 1-sum(MinHashInterests(n1,:)==MinHashInterests(n2,:))/nHF;
+                        if distanceInterests<threshold 
+                            conjunto(count, 1) = n1;
+                            conjunto(count, 2) = n2;
+                            count = count + 1;
+                        end
+                    end
+                end
+            end
+
+            %Contar quantas vezes aparecem cada user que ainda não viu o filme
+            conjuntoUnique = unique(conjunto(:,2));
+            counts = zeros(length(conjuntoUnique),2);
+
+            for i=1:length(conjuntoUnique)
+                u = conjuntoUnique(i);
+                count = 0;
+                for x=1:length(conjunto(:,2))
+                    c = conjunto(x,2);
+                    if c == u
+                        count = count +1;
+                    end
+                end
+                counts(i,1) = u;
+                counts(i,2) = count;
+            end
             
-%             % Queremos os 2 utilizadores que aparecem mais vezes
-%             contagem=zeros(1, length(dic)); 
-%             for i=1:length(SimilarUsersByInterest)
-%                 conjunto=SimilarUsersByInterest{i};
-%                 for j=1:length(conjunto)
-%                     index=double(conjunto{j});
-%                     contagem(index)=contagem(index)+1;
-%                 end
-%             end
-%             [~, indexes]=sort(contagem, 'descend');
-% 
-%             fprintf("\nUsers with most common Sets\n")
-%             % Como são só os primeiros 2, fazemos 2 prints em vez de um for
-%             % é preciso ver o que vem nestes indexes
-%             fprintf("%s\n", dic{Suggestion2Movies(1), 1})
-%             fprintf("%s\n", dic{Suggestion2Movies(2), 1})
-%             fprintf(" (ID: %5d) %s %s\n", userIDs(indexes), dic{indexes,2}, dic{indexes,3})
+            counts = sortrows(counts, 2, "descend");
+
+            mostCommon = counts(1:2); % vai buscar os dois primeiros que são os mais similares
+            fprintf('\nThe two users who appear in more sets are:\n');
+            for i = mostCommon
+                fprintf(" (ID: %d) %s %s\n", i, dic{i,2}, dic{i,3})
+            end
+
         case 4
-        case 5
-            break;
+            
     end
 
     clear menu;
@@ -91,31 +113,4 @@ while(menu ~= 5 && menu ~= 0)
         '3 - Suggestion of users to based on common interests', ...
         '4 - Movies feedback based on populatrity', ...
         '5 - Exit');
-end
-
-%%
-function Set = SimilarInterests(matriz, interesses, threshold)
-    numInter=length(interesses);
-    Set=cell(numInter, 1);
-    for i=1:numInter
-        Set{i}=mostSimilarUsersByInterest(interesses(i), interesses, matriz, threshold);
-    end
-end
-
-function Set = mostSimilarUsersByInterest(interesse, moviesUser, matriz, threshold)
-    Set={};
-    k=1;
-    [lines, col] = size(matriz);
-    for n=1:lines % Aqui onde diz movie, não devia ser o Set gerado no .mat
-        distJaccard=sum(matriz(movie, :)~=matriz(n, :))/col;
-        if distJaccard<threshold && n~=movie % distância de Jaccard inferior e o não ter avaliado o filme atual 
-            if find(moviesUser == n)
-                continue;
-                %se o filme estiver nos avaliados pelo user atual, n faz nada
-            else
-                Set{k}=n;
-                k=k+1;
-            end
-        end
-    end
 end
